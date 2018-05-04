d6e5ce9b43f9316b55c3c22bb2db943cce4eb006
Pavel Kraynyukhov <pk@new-web.co>
2018-05-04 20:39:14 +0600

    auto-build
    Former-commit-id: 07ecb5f0809e5fa5b5b9b5490dd503f74aea2de0

---
+++ inDockerBuild.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export VERSION=$(cat VERSION)
+if [ "${VERSION}x" != "x" ]
+then
+
+  (cd ../ITCLib && git pull) && (cd ../ITCFramework && git pull) && \
+  (cd ../utils && git pull) && git pull && make install-examples clone-luajit clone-libressl && \
+  make build-deb && mv /opt/distrib/lapps-${VERSION}-amd64.deb /opt/lapps/packages/
+
+
+fi
