956b9c1db15f202e885f215902b912ee4e304dad
Pavel Kraynyukhov <pk@new-web.co>
2018-07-03 17:42:53 +0600

    benchmarking fix
    Former-commit-id: f3c8e97b4ea0d0d008393b54cb5c8d85db615b89

--- benchmark/runBenchmark.sh
+++ benchmark/runBenchmark.sh
@@ -1,42 +1,63 @@
 #!/bin/bash
 
+export CLIENTS=96
+export BSIZE=1024
+export URI="wss://localhost:5083/echo"
+
+echo "Usage: $0 [clients [block size [URI]]]"
+
 if [ -f ./echo_client_tls.cpp -a ! -f ./benchmark_tls ]
 then
   g++ -pthread -O3 -std=c++14 echo_client_tls.cpp -o benchmark_tls -lboost_system -lcrypto -lssl
 fi
 
-if [ -f ./echo_client.cpp -a ! -f ./benchmark_notls ]
-then
-  g++ -pthread -O3 -std=c++14 echo_client.cpp -o benchmark_notls -lboost_system
-fi
+LAPPS_PID=$(ps ax | grep lapps | grep -v grep | awk '{print $1}')
 
-export clients=80
 
 if [ "${1}x" != "x" ]
 then
-  clients=$1
+  CLIENTS=$1
 fi
 
-rm log.*; let j=0; while [ $j -lt $clients ]; do ./benchmark_tls > log.$j & let j++; done
+if [ "${2}x" != "x" ]
+then
+  BSIZE=$2
+fi
 
-sleep 3;
+if [ "${3}x" != "x" ]
+then
+  URI=$3
+fi
 
-clients_started=$(ps ax | grep benchmark | grep -v grep | wc -l)
+if [ "${LAPPS_PID}x" == "x" ]
+then
+  echo LAPPS is not started on localhost. benchmarking uri is $URI 
+fi
 
-echo clients started: $clients_started
+echo Running $CLIENTS echo-processes with $BSIZE bytes large messages
 
-while [ 1 ]
+rm -f log.*; let j=0;while [ $j -lt $CLIENTS ]; do ./benchmark_tls $URI $BSIZE >log.$j & let j++; done 
+
+
+sleep 10
+
+let j=0;
+while [ $j -lt 300 ]
 do
-  count_bms=$(ps ax | grep benchmark | grep -v grep | wc -l)
-  if [ $count_bms -eq 0 ]
-  then
-    break;
-  else
-    sleep 1;
-  fi
+  egrep "^Mess" log.* | sed -e 's/ms$//g' | awk -v rpsavg=0 -v latency_avg=0 -v clients=$CLIENTS -v bsize=$BSIZE '{ rpsavg=rpsavg+($2-rpsavg)/(NR);latency_avg=latency_avg+($4-latency_avg)/(NR);}END{print "clients:",clients,"total rps:", (rpsavg/latency_avg)*1000*clients, "rp-per-snapshot:", rpsavg, "avg-snap-time:", latency_avg, "rps/c:", (rpsavg/latency_avg)*1000, "MBit/s:", ((bsize*(rpsavg/latency_avg)*1000)*clients*8)/1000000}';
+  sleep 10
+  let j+=10
 done
 
-echo "All results bellow are for ${clients_started} clients"
-echo
+pkill benchmark_tls
 
-grep tabdata log.* | awk '{print $2, $3}' | awk '{count[$1]++; avg[$1]=avg[$1]+($2-avg[$1])/(count[$1]+1)}END{for(i in avg) print avg[i]*count[i], i, avg[i], i*avg[i]*count[i]/1024/1024*8 " Mbit/s, clients: " count[i]}' | sort -nr
+LAPPS_PID=$(ps ax | grep lapps | egrep -v "grep|gdb" | awk '{print $1}')
+
+if [ "${LAPPS_PID}x" != "x" ]
+then
+  kill -15 $LAPPS_PID
+else
+  echo "No LAppS to stop"
+fi
+
+egrep "^Mess" log.* | sed -e 's/ms$//g' | awk -v rpsavg=0 -v latency_avg=0 -v clients=$CLIENTS -v bsize=$BSIZE '{ rpsavg=rpsavg+($2-rpsavg)/(NR);latency_avg=latency_avg+($4-latency_avg)/(NR);}END{print "clients:",clients,"total rps:", (rpsavg/latency_avg)*1000*clients, "rp-per-snapshot:", rpsavg, "avg-snap-time:", latency_avg, "rps/c:", (rpsavg/latency_avg)*1000, "MBit/s:", ((bsize*(rpsavg/latency_avg)*1000)*clients*8)/1000000}';
