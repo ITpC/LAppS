b7e260638f472afa59bf4f05f87a8d61434283c6
Pavel Kraynyukhov <pk@new-web.co>
2018-07-16 14:59:15 +0600

    build sequence fix

--- inDockerBuild.ssse3.sh
+++ inDockerBuild.ssse3.sh
@@ -5,7 +5,7 @@ if [ "${VERSION}x" != "x" ]
 then
 
   (cd ../ITCLib && git pull) && (cd ../ITCFramework && git pull) && \
-  (cd ../utils && git pull) && git pull && make CONF=Release.GENERIC install-examples clone-luajit clone-libressl && \
+  (cd ../utils && git pull) && git pull && make CONF=Release.GENERIC clean build install-examples clone-luajit clone-libressl && \
   make build-deb && mv /opt/distrib/lapps-${VERSION}-amd64.deb /opt/lapps/packages/
 
 fi
