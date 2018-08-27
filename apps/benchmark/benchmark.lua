523a991cc692d804a6ba405466aba6b393fab4d4
Pavel Kraynyukhov <pk@new-web.co>
2018-08-27 14:45:10 +0600

    benchmark service which uses cws module

---
+++ apps/benchmark/benchmark.lua
@@ -0,0 +1,56 @@
+benchmark={}
+benchmark.__index=benchmark
+
+benchmark.init=function()
+end
+
+benchmark.messages_counter=0;
+benchmark.start_time=time.now();
+
+benchmark.meter=function()
+  benchmark.messages_counter=benchmark.messages_counter+1;
+
+  local slice=time.now() - benchmark.start_time;
+  if( slice  >= 1000)
+  then
+    print(benchmark.messages_counter.." messages receved per "..slice.."ms")
+    benchmark.messages_counter=0;
+    benchmark.start_time=time.now();
+  end
+end
+
+benchmark.run=function()
+  local array={};
+  for i=0,100
+  do
+    array[i]=cws:new(
+      "wss://127.0.0.1:5083/echo",
+      {
+        ["onopen"]=function(handler)
+          local result, errstr=cws:send(handler,"xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",2);
+          if(not result)
+          then
+            print("Error on websocket send at handler "..handler..": "..errstr);
+          end
+        end,
+        ["onmessage"]=function(handler,message,opcode)
+          benchmark.meter();
+          cws:send(handler,message,opcode);
+        end,
+        ["onerror"]=function(handler, message)
+          print(message..". Socket FD:  "..handler);
+        end,
+        ["onclose"]=function(handler)
+          print("WebSocket "..handler.." is closed by peer.");
+        end
+      });
+  end
+  benchmark.start_time=time.now();
+  while not must_stop()
+  do
+    cws:eventLoop();
+  end
+end
+
+return benchmark;
+
