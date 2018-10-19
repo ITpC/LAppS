0d2d0ac661d2672271dc91e33c186c3a62052494
Pavel Kraynyukhov <pk@new-web.co>
2018-10-19 19:27:38 +0600

    fix for VERSION reading
    Former-commit-id: 55c0b2128df01b44cff6154f31ef69521e432bdf

--- inDockerPackage.ssse3.sh
+++ inDockerPackage.ssse3.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+export VERSION=$(cat VERSION)
+
 ./inDockerBuild.ssse3.sh && make CONF=Release.GENERIC build-deb && mv /opt/distrib/lapps-${VERSION}-amd64.deb /opt/lapps/packages/lapps-${VERSION}-ssse3-amd64.deb
