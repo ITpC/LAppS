b9f152b0a6c897ce49aac92191c6c44cad0dd513
Pavel Kraynyukhov <pk@new-web.co>
2018-10-19 19:03:25 +0600

    proper package names on copy
    Former-commit-id: bf40b2963721f2b7a67936df6d8d50da868664a0

--- inDockerPackage.sh
+++ inDockerPackage.sh
@@ -2,4 +2,4 @@
 
 export VERSION=$(cat VERSION)
 
-./inDockerBuild.sh && make CONF=Release.AVX2 build-deb && mv /opt/distrib/lapps-${VERSION}-avx2-amd64.deb /opt/lapps/packages/
+./inDockerBuild.sh && make CONF=Release.AVX2 build-deb && mv /opt/distrib/lapps-${VERSION}-amd64.deb /opt/lapps/packages/lapps-${VERSION}-avx2-amd64.deb
