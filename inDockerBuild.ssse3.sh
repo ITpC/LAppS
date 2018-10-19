5b16cb19ea29b55063ff9655a75d91217d06de09
Pavel Kraynyukhov <pk@new-web.co>
2018-10-19 13:55:53 +0600

    Packaging separation, as per fr: #24
    Former-commit-id: 3c52d908484f109ccd7ed318ae2d280a8f0435d6

--- inDockerBuild.ssse3.sh
+++ inDockerBuild.ssse3.sh
@@ -5,7 +5,9 @@ if [ "${VERSION}x" != "x" ]
 then
 
   (cd ../ITCLib && git pull) && (cd ../ITCFramework && git pull) && \
-  (cd ../utils && git pull) && (cd ../lar && git pull && make install)  && git pull && make CONF=Release.GENERIC clean build install-examples clone-luajit clone-libressl && \
-  make build-deb && mv /opt/distrib/lapps-${VERSION}-amd64.deb /opt/lapps/packages/
+  (cd ../utils && git pull) && (cd ../lar && git pull && make install)  && git pull &&\
+  make CONF=Release.GENERIC clean build &&\
+  make CONF=Release.GENERIC.NO_STATS build &&\
+  make CONF=Release.GENERIC.NO_STATS.NO_TLS install-examples clone-luajit clone-libressl
 
 fi
