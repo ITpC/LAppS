50645d4e7fa48f46de6d245118e63ad39e1616d9
Pavel Kraynyukhov <pk@new-web.co>
2018-10-19 20:05:37 +0600

    better syntax
    Former-commit-id: 401f9fb5d7109fce645775d069cd38fc837b5c91

--- examples/echo/echo.lua
+++ examples/echo/echo.lua
@@ -2,18 +2,18 @@ echo = {}
 
 echo.__index = echo;
 
-echo["onStart"]=function()
+echo.onStart=function()
   print("echo::onStart");
 end
 
-echo["onDisconnect"]=function()
+echo.onDisconnect=function()
 end
 
-echo["onShutdown"]=function()
+echo.onShutdown=function()
   print("echo::onShutdown()");
 end
 
-echo["onMessage"]=function(handler,opcode, message)
+echo.onMessage=function(handler,opcode, message)
   -- print("echo::onMessage");
   
   local result, errmsg=ws:send(handler,opcode,message);
