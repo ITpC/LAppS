c90e96e6ee0b268a7599e0347e3970c7d64f2e16
Pavel Kraynyukhov <pk@new-web.co>
2018-10-20 15:09:08 +0600

    builds separation, cleanup before build fix

--- inDockerBuild.avx2.sh
+++ inDockerBuild.avx2.sh
@@ -4,12 +4,13 @@ export VERSION=$(cat VERSION)
 if [ "${VERSION}x" != "x" ]
 then
 
+  rm -f /opt/lapps/bin/lapps*
+
   (cd ../ITCLib && git pull) && (cd ../ITCFramework && git pull) && \
   (cd ../utils && git pull) && (cd ../lar && git pull && make install) && git pull &&\
-  rm -f /opt/lapps/bin/lapps* &&\
-  make CONF=Release.AVX2 clean build &&\
-  make CONF=Release.AVX2.NO_STATS build &&\
-  make CONF=Release.AVX2.NO_STATS.NO_TLS build &&\
+  make CONF=Release.AVX2 clean build install &&\
+  make CONF=Release.AVX2.NO_STATS build install &&\
+  make CONF=Release.AVX2.NO_STATS.NO_TLS build install &&\
   make CONF=Release.AVX2.NO_STATS.NO_TLS install-examples clone-luajit clone-libressl
 
 fi
--- inDockerBuild.generic.sh
+++ inDockerBuild.generic.sh
@@ -4,11 +4,11 @@ export VERSION=$(cat VERSION)
 if [ "${VERSION}x" != "x" ]
 then
 
+  rm -f /opt/lapps/bin/lapps* 
   (cd ../ITCLib && git pull) && (cd ../ITCFramework && git pull) && \
   (cd ../utils && git pull) && (cd ../lar && git pull && make install)  && git pull &&\
-  rm -f /opt/lapps/bin/lapps* &&\
-  make CONF=Release.GENERIC clean build &&\
-  make CONF=Release.GENERIC.NO_STATS build &&\
-  make CONF=Release.GENERIC.NO_STATS.NO_TLS install-examples clone-luajit clone-libressl
+  make CONF=Release.GENERIC clean build install &&\
+  make CONF=Release.GENERIC.NO_STATS build install &&\
+  make CONF=Release.GENERIC.NO_STATS.NO_TLS build install install-examples clone-luajit clone-libressl
 
 fi
