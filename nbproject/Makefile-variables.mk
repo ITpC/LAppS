8323d39aa95cfafab8d8fc6d63c8181276097ff3
Pavel Kraynyukhov <pk@new-web.co>
2018-11-04 19:34:04 +0600

    fix: #62 providing sse2 builds for 0.7.2
    Former-commit-id: 19e7e84fbeaa8733636c81d61bea5236ce706f97

--- nbproject/Makefile-variables.mk
+++ nbproject/Makefile-variables.mk
@@ -14,14 +14,14 @@ CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux/lapps
 CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux/package
 CND_PACKAGE_NAME_Debug=lapps.deb
 CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux/package/lapps.deb
-# Release.GENERIC configuration
-CND_PLATFORM_Release.GENERIC=GNU-Linux
-CND_ARTIFACT_DIR_Release.GENERIC=dist/Release.GENERIC/GNU-Linux
-CND_ARTIFACT_NAME_Release.GENERIC=lapps.generic
-CND_ARTIFACT_PATH_Release.GENERIC=dist/Release.GENERIC/GNU-Linux/lapps.generic
-CND_PACKAGE_DIR_Release.GENERIC=dist/Release.GENERIC/GNU-Linux/package
-CND_PACKAGE_NAME_Release.GENERIC=lapps.deb
-CND_PACKAGE_PATH_Release.GENERIC=dist/Release.GENERIC/GNU-Linux/package/lapps.deb
+# Release.SSSE3 configuration
+CND_PLATFORM_Release.SSSE3=GNU-Linux
+CND_ARTIFACT_DIR_Release.SSSE3=dist/Release.SSSE3/GNU-Linux
+CND_ARTIFACT_NAME_Release.SSSE3=lapps.sse3
+CND_ARTIFACT_PATH_Release.SSSE3=dist/Release.SSSE3/GNU-Linux/lapps.sse3
+CND_PACKAGE_DIR_Release.SSSE3=dist/Release.SSSE3/GNU-Linux/package
+CND_PACKAGE_NAME_Release.SSSE3=lapps.deb
+CND_PACKAGE_PATH_Release.SSSE3=dist/Release.SSSE3/GNU-Linux/package/lapps.deb
 # Release.AVX2 configuration
 CND_PLATFORM_Release.AVX2=GNU-Linux
 CND_ARTIFACT_DIR_Release.AVX2=dist/Release.AVX2/GNU-Linux
@@ -30,22 +30,22 @@ CND_ARTIFACT_PATH_Release.AVX2=dist/Release.AVX2/GNU-Linux/lapps.avx2
 CND_PACKAGE_DIR_Release.AVX2=dist/Release.AVX2/GNU-Linux/package
 CND_PACKAGE_NAME_Release.AVX2=lapps.deb
 CND_PACKAGE_PATH_Release.AVX2=dist/Release.AVX2/GNU-Linux/package/lapps.deb
-# Release.GENERIC.NO_STATS configuration
-CND_PLATFORM_Release.GENERIC.NO_STATS=GNU-Linux
-CND_ARTIFACT_DIR_Release.GENERIC.NO_STATS=dist/Release.GENERIC.NO_STATS/GNU-Linux
-CND_ARTIFACT_NAME_Release.GENERIC.NO_STATS=lapps.generic.nostats
-CND_ARTIFACT_PATH_Release.GENERIC.NO_STATS=dist/Release.GENERIC.NO_STATS/GNU-Linux/lapps.generic.nostats
-CND_PACKAGE_DIR_Release.GENERIC.NO_STATS=dist/Release.GENERIC.NO_STATS/GNU-Linux/package
-CND_PACKAGE_NAME_Release.GENERIC.NO_STATS=lapps.deb
-CND_PACKAGE_PATH_Release.GENERIC.NO_STATS=dist/Release.GENERIC.NO_STATS/GNU-Linux/package/lapps.deb
-# Release.GENERIC.NO_STATS.NO_TLS configuration
-CND_PLATFORM_Release.GENERIC.NO_STATS.NO_TLS=GNU-Linux
-CND_ARTIFACT_DIR_Release.GENERIC.NO_STATS.NO_TLS=dist/Release.GENERIC.NO_STATS.NO_TLS/GNU-Linux
-CND_ARTIFACT_NAME_Release.GENERIC.NO_STATS.NO_TLS=lapps.generic.nostats.notls
-CND_ARTIFACT_PATH_Release.GENERIC.NO_STATS.NO_TLS=dist/Release.GENERIC.NO_STATS.NO_TLS/GNU-Linux/lapps.generic.nostats.notls
-CND_PACKAGE_DIR_Release.GENERIC.NO_STATS.NO_TLS=dist/Release.GENERIC.NO_STATS.NO_TLS/GNU-Linux/package
-CND_PACKAGE_NAME_Release.GENERIC.NO_STATS.NO_TLS=lapps.deb
-CND_PACKAGE_PATH_Release.GENERIC.NO_STATS.NO_TLS=dist/Release.GENERIC.NO_STATS.NO_TLS/GNU-Linux/package/lapps.deb
+# Release.SSSE3.NO_STATS configuration
+CND_PLATFORM_Release.SSSE3.NO_STATS=GNU-Linux
+CND_ARTIFACT_DIR_Release.SSSE3.NO_STATS=dist/Release.SSSE3.NO_STATS/GNU-Linux
+CND_ARTIFACT_NAME_Release.SSSE3.NO_STATS=lapps.ssse3.nostats
+CND_ARTIFACT_PATH_Release.SSSE3.NO_STATS=dist/Release.SSSE3.NO_STATS/GNU-Linux/lapps.ssse3.nostats
+CND_PACKAGE_DIR_Release.SSSE3.NO_STATS=dist/Release.SSSE3.NO_STATS/GNU-Linux/package
+CND_PACKAGE_NAME_Release.SSSE3.NO_STATS=lapps.deb
+CND_PACKAGE_PATH_Release.SSSE3.NO_STATS=dist/Release.SSSE3.NO_STATS/GNU-Linux/package/lapps.deb
+# Release.SSSE3.NO_STATS.NO_TLS configuration
+CND_PLATFORM_Release.SSSE3.NO_STATS.NO_TLS=GNU-Linux
+CND_ARTIFACT_DIR_Release.SSSE3.NO_STATS.NO_TLS=dist/Release.SSSE3.NO_STATS.NO_TLS/GNU-Linux
+CND_ARTIFACT_NAME_Release.SSSE3.NO_STATS.NO_TLS=lapps.sse3.nostats.notls
+CND_ARTIFACT_PATH_Release.SSSE3.NO_STATS.NO_TLS=dist/Release.SSSE3.NO_STATS.NO_TLS/GNU-Linux/lapps.sse3.nostats.notls
+CND_PACKAGE_DIR_Release.SSSE3.NO_STATS.NO_TLS=dist/Release.SSSE3.NO_STATS.NO_TLS/GNU-Linux/package
+CND_PACKAGE_NAME_Release.SSSE3.NO_STATS.NO_TLS=lapps.deb
+CND_PACKAGE_PATH_Release.SSSE3.NO_STATS.NO_TLS=dist/Release.SSSE3.NO_STATS.NO_TLS/GNU-Linux/package/lapps.deb
 # Release.AVX2.NO_STATS configuration
 CND_PLATFORM_Release.AVX2.NO_STATS=GNU-Linux
 CND_ARTIFACT_DIR_Release.AVX2.NO_STATS=dist/Release.AVX2.NO_STATS/GNU-Linux
@@ -70,6 +70,30 @@ CND_ARTIFACT_PATH_Debug.AVX2.NO_STATS.NO_TLS=dist/Debug.AVX2.NO_STATS.NO_TLS/GNU
 CND_PACKAGE_DIR_Debug.AVX2.NO_STATS.NO_TLS=dist/Debug.AVX2.NO_STATS.NO_TLS/GNU-Linux/package
 CND_PACKAGE_NAME_Debug.AVX2.NO_STATS.NO_TLS=lapps.deb
 CND_PACKAGE_PATH_Debug.AVX2.NO_STATS.NO_TLS=dist/Debug.AVX2.NO_STATS.NO_TLS/GNU-Linux/package/lapps.deb
+# Release.SSE2 configuration
+CND_PLATFORM_Release.SSE2=GNU-Linux
+CND_ARTIFACT_DIR_Release.SSE2=dist/Release.SSE2/GNU-Linux
+CND_ARTIFACT_NAME_Release.SSE2=lapps.sse2
+CND_ARTIFACT_PATH_Release.SSE2=dist/Release.SSE2/GNU-Linux/lapps.sse2
+CND_PACKAGE_DIR_Release.SSE2=dist/Release.SSE2/GNU-Linux/package
+CND_PACKAGE_NAME_Release.SSE2=lapps.deb
+CND_PACKAGE_PATH_Release.SSE2=dist/Release.SSE2/GNU-Linux/package/lapps.deb
+# Release.SSE2.NO_STATS configuration
+CND_PLATFORM_Release.SSE2.NO_STATS=GNU-Linux
+CND_ARTIFACT_DIR_Release.SSE2.NO_STATS=dist/Release.SSE2.NO_STATS/GNU-Linux
+CND_ARTIFACT_NAME_Release.SSE2.NO_STATS=lapps.sse2.nostats
+CND_ARTIFACT_PATH_Release.SSE2.NO_STATS=dist/Release.SSE2.NO_STATS/GNU-Linux/lapps.sse2.nostats
+CND_PACKAGE_DIR_Release.SSE2.NO_STATS=dist/Release.SSE2.NO_STATS/GNU-Linux/package
+CND_PACKAGE_NAME_Release.SSE2.NO_STATS=lapps.deb
+CND_PACKAGE_PATH_Release.SSE2.NO_STATS=dist/Release.SSE2.NO_STATS/GNU-Linux/package/lapps.deb
+# Release.SSE2.NO_STATS.NO_TLS configuration
+CND_PLATFORM_Release.SSE2.NO_STATS.NO_TLS=GNU-Linux
+CND_ARTIFACT_DIR_Release.SSE2.NO_STATS.NO_TLS=dist/Release.SSE2.NO_STATS.NO_TLS/GNU-Linux
+CND_ARTIFACT_NAME_Release.SSE2.NO_STATS.NO_TLS=lapps.sse2.nostats.notls
+CND_ARTIFACT_PATH_Release.SSE2.NO_STATS.NO_TLS=dist/Release.SSE2.NO_STATS.NO_TLS/GNU-Linux/lapps.sse2.nostats.notls
+CND_PACKAGE_DIR_Release.SSE2.NO_STATS.NO_TLS=dist/Release.SSE2.NO_STATS.NO_TLS/GNU-Linux/package
+CND_PACKAGE_NAME_Release.SSE2.NO_STATS.NO_TLS=lapps.deb
+CND_PACKAGE_PATH_Release.SSE2.NO_STATS.NO_TLS=dist/Release.SSE2.NO_STATS.NO_TLS/GNU-Linux/package/lapps.deb
 #
 # include compiler specific variables
 #
