f5f6bd25d59af46d9e6769acc1bcfd7bc5daa732
Pavel Kraynyukhov <pk@new-web.co>
2019-06-23 13:29:32 +0600

    `cinq_depth option\' removed; epoll policies are added: `max_poll_events\' - max amount of events polled at once; `max_poll_wait_ms\' maximum wait time in ms spent in epoll at once

--- include/Config.h
+++ include/Config.h
@@ -49,9 +49,8 @@ namespace LAppS
       {"connection_weight", 1.0},
       {"ip","0.0.0.0"},
       {"port",5083},
-      {"cinq_depth",1000},
       {"lapps_config_auto_save", true },
-      {"workers",{ {"workers",4}, {"max_connections", 10000 },{"auto_fragment",false}}},
+      {"workers",{ {"workers",4}, {"max_connections", 10000 },{"auto_fragment",false},{"max_poll_events",300},{"max_poll_wait_ms",10}}},
       {"acl", {{"policy", "allow"},{"exclude", {} }}},
 #ifdef LAPPS_TLS_ENABLE
       {"tls",true},
