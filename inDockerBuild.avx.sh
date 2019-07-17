1948b92b9a186d104751fb10957d97c8bdfeadf5
Pavel Kraynyukhov <pk@new-web.co>
2019-07-17 10:04:19 +0600

    WIP towards 0.8.0 release

--- inDockerBuild.avx.sh
+++ inDockerBuild.avx.sh
@@ -8,11 +8,11 @@ then
 
   (cd ../ITCLib && git pull) && (cd ../ITCFramework && git pull) && \
   (cd ../utils && git pull) && (cd ../lar && git pull && make install) && git pull &&\
-  make CONF=Release.AVX2 clean &&\
-  make CONF=Release.AVX2 build install &&\
-  make CONF=Release.AVX2.NO_STATS build install &&\
-  make CONF=Release.AVX2.NO_STATS.NO_TLS build install &&\
-  make CONF=Release.AVX2.NO_STATS.NO_TLS install-examples clone-usr-local
+  make CONF=Release.AVX clean &&\
+  make CONF=Release.AVX build install &&\
+  make CONF=Release.AVX.NO_STATS build install &&\
+  make CONF=Release.AVX.NO_STATS.NO_TLS build install &&\
+  make CONF=Release.AVX.NO_STATS.NO_TLS install-examples clone-usr-local
 
   ls -la /opt/lapps/bin
 
