822fe28f0b2fb6a3fa9be57a54103030bd9df70d
Pavel Kraynyukhov <pk@new-web.co>
2019-07-17 15:10:14 +0600

    fix for binary names of the AVX build. 0.8.0-RC2

--- nbproject/Package-Release.AVX.NO_STATS.NO_TLS.bash
+++ nbproject/Package-Release.AVX.NO_STATS.NO_TLS.bash
@@ -13,8 +13,8 @@ CND_BUILDDIR=build
 CND_DLIB_EXT=so
 NBTMPDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}/tmp-packaging
 TMPDIRNAME=tmp-packaging
-OUTPUT_PATH=${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/lapps.avx2.nostats.notls
-OUTPUT_BASENAME=lapps.avx2.nostats.notls
+OUTPUT_PATH=${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/lapps.avx.nostats.notls
+OUTPUT_BASENAME=lapps.avx.nostats.notls
 PACKAGE_TOP_DIR=/usr/
 
 # Functions
