4f37cb3ebb63b2b7e612091f748c50849325fa40
Pavel Kraynyukhov <pk@new-web.co>
2021-01-01 06:49:45 +0600

    email changed
    Former-commit-id: 623bec912d9cfb97325239028aef84b7d59fa73d

--- include/abstract/WebSocket.h
+++ include/abstract/WebSocket.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018 Pavel Kraynyukhov <pk@new-web.co>
+ * Copyright (C) 2018 Pavel Kraynyukhov <pavel.kraynyukhov@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
